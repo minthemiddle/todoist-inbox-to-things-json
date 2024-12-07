9463ac57d8a885151d17b80aba713a83379b391b
Martin Betz <martinmichaelbetz@gmail.com>
2024-12-07 19:25:51 +0100

    chore: Require Composer's autoload file in todoist-to-things.php

--- todoist-to-things.php
+++ todoist-to-things.php
@@ -1,5 +1,8 @@
 <?php
 
+// Require Composer's autoload file
+ require 'vendor/autoload.php';
+
 // Load environment variables from .env file
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
